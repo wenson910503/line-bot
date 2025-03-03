8fa9da81c5182da94a3132a425238a817f51350a
wenson <71477300+wenson910503@users.noreply.github.com>
2025-03-03 21:24:57 +0800

    Update line-botDining room GPT.py

--- line-botDining room GPT.py
+++ line-botDining room GPT.py
@@ -10,14 +10,14 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 # 🚀 填入你的 LINE Bot API Key
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', '')
-LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET', '')
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('i8DEpkz7jgRNnqRR4mWbPxC5oesrSpXbw2c+5xpzkLASeiBvdtv1uny/4/iXeO4lJygtxMZylP6IlFmQq/Lva/Ftd/H05aGKjTFlHZ3iSZo1sEMmBKRVMTTemEtU0zKtk9S9nqXIGc8CnOWSS80zKAdB04t89/1O/w1cDnyilFU=')
+LINE_CHANNEL_SECRET = os.getenv('e95d4cac941b6109c3379f5cb7a7c46c')
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 # 🚀 填入你的 Google Places API Key
-GOOGLE_PLACES_API_KEY = os.getenv('GOOGLE_PLACES_API_KEY', '')
+GOOGLE_PLACES_API_KEY = os.getenv('AIzaSyBqbjGjjpt3Bxo9RB15DE4uVBmoBRlNXVM')
 
 # 📍 Google Places API 查詢函數
 def search_restaurants(location):
