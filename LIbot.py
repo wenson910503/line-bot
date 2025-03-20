1efe99a73d18a669d83e3659b44d57fd6556853d
wenson <71477300+wenson910503@users.noreply.github.com>
2025-03-20 19:41:25 +0800

    Update LIbot.py

--- LIbot.py
+++ LIbot.py
@@ -8,11 +8,11 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 app = Flask(__name__)
 
 # 🚀 填入你的 LINE Bot API Key
-line_bot_api = LineBotApi('YOUR_LINE_BOT_API_KEY')
-handler = WebhookHandler('YOUR_LINE_SECRET')
+line_bot_api = LineBotApi('i8DEpkz7jgRNnqRR4mWbPxC5oesrSpXbw2c+5xpzkLASeiBvdtv1uny/4/iXeO4lJygtxMZylP6IlFmQq/Lva/Ftd/H05aGKjTFlHZ3iSZo1sEMmBKRVMTTemEtU0zKtk9S9nqXIGc8CnOWSS80zKAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('e95d4cac941b6109c3379f5cb7a7c46c')
 
 # 🚀 填入你的 Google Places API Key
-GOOGLE_PLACES_API_KEY = 'YOUR_GOOGLE_PLACES_API_KEY'
+GOOGLE_PLACES_API_KEY = 'AIzaSyBqbjGjjpt3Bxo9RB15DE4uVBmoBRlNXVM'
 
 # 📍 Google Places API 查詢函數（顯示最多 3 間餐廳，包含評論與圖片）
 def search_restaurants(location):
